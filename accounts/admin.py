5ded89033f383acdcafcd5c98510f6e2a0903db5
my3rdstory <my3rdstory@gmail.com>
2025-06-25 08:16:34 +0900

    장고어드민 사용자 메뉴에 라이트닝 로그인 연동 여부 표시

--- accounts/admin.py
+++ accounts/admin.py
@@ -48,3 +48,25 @@ admin.site.unregister(User)
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     inlines = (LightningUserInline,)
+    list_per_page = 10  # 페이지당 10개씩 표시
+    
+    # 기존 list_display에 라이트닝 연동 상태 추가
+    list_display = UserAdmin.list_display + ('lightning_status',)
+    
+    def lightning_status(self, obj):
+        """라이트닝 연동 상태 표시"""
+        try:
+            lightning_user = obj.lightning_profile
+            if lightning_user:
+                return "⚡ 연동됨"
+            else:
+                return "❌ 미연동"
+        except LightningUser.DoesNotExist:
+            return "❌ 미연동"
+    
+    lightning_status.short_description = '라이트닝 연동'
+    lightning_status.admin_order_field = 'lightning_profile'
+    
+    def get_queryset(self, request):
+        """쿼리 최적화 - 라이트닝 프로필 정보를 미리 로드"""
+        return super().get_queryset(request).select_related('lightning_profile')
